0982c1bd2fc57dc05f1d9d00956646114aa6edc5
brandbandhu <brandbandhu.praavi@gmail.com>
2026-08-18 12:03:08 +0530

    Add ScrollToTop behavior on route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { Toaster } from "sonner";
 import { StoreProvider } from "@/lib/store";
 import { Navbar } from "@/components/site/Navbar";
@@ -24,9 +25,20 @@ import BlogPage from "@/routes/blog";
 import BlogPostPage from "@/routes/blog.$slug";
 import ContactPage from "@/routes/contact";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "instant" as ScrollBehavior });
+  }, [pathname]);
+
+  return null;
+}
+
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <StoreProvider>
         <div className="relative flex min-h-screen flex-col">
           <Navbar />
